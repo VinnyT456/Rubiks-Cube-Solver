a8f13cf59e7dbff2442783c5b52289b895c52b5a
Vinny456 <vincent03280608@gmail.com>
2024-06-05 18:20:26 -0400

    Improved Tile Detection

--- scan.py
+++ scan.py
@@ -13,13 +13,14 @@ class Cube_Scanner:
             "white": (213, 220, 200)
         }
         self.tiles = []
+        self.frame = None
         self.cam = cv2.VideoCapture(0)
         self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
         self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
         self.width = int(self.cam.get(cv2.CAP_PROP_FRAME_WIDTH))
         self.height = int(self.cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    def classify_tile_color(self, color):
+    def classify_tile_color(self, color) -> str:
         min_dist = float('inf')
         closest_color = None
         for color_name, color_value in self.color_pattern.items():
@@ -29,16 +30,16 @@ class Cube_Scanner:
                 closest_color = color_name
         return closest_color
 
-    def get_average_color(self, tile):
-        pixels = tile.reshape((-1, 3))
+    def get_average_color(self) -> tuple:
+        pixels = self.tile.reshape((-1, 3))
         kmeans = KMeans(n_clusters=1, n_init="auto")
         kmeans.fit(pixels)
         dominant_color = kmeans.cluster_centers_[0]
         dominant_color_bgr = tuple(int(c) for c in dominant_color)
         return dominant_color_bgr
     
-    def cube_preprocess(self, image):
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    def cube_preprocess(self) -> None:
+        gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
         blurred = cv2.GaussianBlur(gray, (15, 15), 2)
         canny = cv2.Canny(blurred, 20, 40)
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9))
@@ -46,8 +47,8 @@ class Cube_Scanner:
         thresh = cv2.threshold(dilated, 0, 255, cv2.THRESH_BINARY)[1]
         return thresh
     
-    def find_stickers(self, image):
-        new_image = self.cube_preprocess(image)
+    def find_stickers(self) -> None:
+        new_image = self.cube_preprocess()
         contours, _ = cv2.findContours(new_image.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
         contour_areas = [cv2.contourArea(cnt) for cnt in contours]
@@ -74,36 +75,38 @@ class Cube_Scanner:
             if 0.8 <= aspect_ratio <= 1.2:
                 self.tiles.append((x, y, w, h))
         
-        if (len(self.tiles) == 9):
-            self.draw_tiles(image)
+        if (len(self.tiles) == 9 and self.check_valid_tiles()):
+            self.draw_tiles()
 
-    def scan(self):
+    def check_valid_tiles(self) -> bool:
+        self.sort_tiles()
+        grouped_tiles = np.array([self.tiles[i:i+3] for i in range(0, len(self.tiles), 3)])
+        for group in grouped_tiles:
+            avg = np.mean(group, axis=0)
+            for x in group:
+                if (x[0] < avg[0] - 10) or (x[0] > avg[0] + 10):
+                    return False
+        return True
+    
+    def scan(self) -> None:
         while True:
-            ret, frame = self.cam.read()
+            ret, self.frame = self.cam.read()
             if not ret:
                 break
-            self.find_stickers(frame)
-            cv2.imshow("Image", frame)
-            self.center_window("Image")
+            self.find_stickers()
+            cv2.imshow("Image", self.frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
-    def sort_tiles(self):
+    def sort_tiles(self) -> None:
         self.tiles.sort(key=lambda tile: tile[0])
         grouped_tiles = [self.tiles[i:i+3] for i in range(0, len(self.tiles), 3)]
         sorted_tile = [sorted(group, key=lambda x: x[1], reverse=True) for group in grouped_tiles]
         self.tiles = [tile for group in sorted_tile for tile in group]
 
-    def draw_tiles(self, image):
+    def draw_tiles(self) -> None:
         for tile in self.tiles:
-            cv2.rectangle(image, (tile[0], tile[1]), (tile[0]+tile[2], tile[1]+tile[3]), (0, 255, 0), 2)
-
-    def center_window(self, window_name):
-        screen_width = cv2.getWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN)
-        screen_height = cv2.getWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN)
-        if screen_width > 0 and screen_height > 0:
-            cv2.moveWindow(window_name, int(screen_width/2 - self.width/2), int(screen_height/2 - self.height/2))
-
+            cv2.rectangle(self.frame, (tile[0], tile[1]), (tile[0]+tile[2], tile[1]+tile[3]), (0, 255, 0),5)
 if __name__ == "__main__":
     cube_scanner = Cube_Scanner()
     cube_scanner.scan()
