75c8bb9ba98dfc360d159ec1c03c12da5da1179a
Vinny456 <vincent03280608@gmail.com>
2025-09-02 18:45:22 -0400

    Initialize the words to describe each move on the cube

---
+++ solution.py
@@ -0,0 +1,29 @@
+algorithm_word_conversion = {
+    "R":"Rotate the right side of the cube once in the clockwise direction",
+    "R'":"Rotate the right side of the cube once in the counter-clockwise direction",
+    "R2":"Rotate the right side of the cube twice in any direction",
+    "L":"Rotate the left side of the cube once in the clockwise direction",
+    "L'":"Rotate the left side of the cube once in the counter-clockwise direction",
+    "L2":"Rotate the left side of the cube twice in any direction",
+    "F":"Rotate the front side of the cube once in the clockwise direction",
+    "F'":"Rotate the front of the cube once in the counter-clockwise direction",
+    "F2":"Rotate the front of the cube twice in any direction",
+    "B":"Rotate the back of the cube once in the clockwise direction",
+    "B'":"Rotate the back of the cube once in the counter-clockwise direction",
+    "B2":"Rotate the back of the cube twice in any direction",
+    "U":"Rotate the top of the cube once in the clockwise direction",
+    "U'":"Rotate the top side of the cube once in the counter-clockwise direction",
+    "U2":"Rotate the top side of the cube twice in any direction",
+    "D":"Rotate the bottom of the cube once in the clockwise direction",
+    "D'":"Rotate the bottom of the cube once in the counter-clockwise direction",
+    "D2":"Rotate the bottom of the cube twice in any direction",
+    "M":"Rotate the middle of the cube once in the clockwise direction",
+    "M'":"Rotate the middle of the cube once in the counter-clockwise direction",
+    "M2":"Rotate the middle of the cube twice in any direction",
+    "y":"Rotate the whole cube once to the right",
+    "y'":"Rotate the whole cube once to the left",
+    "y2":"Rotate the whole cube twice in any direction",
+}
+
+def algorithm_to_word(move):
+    pass
