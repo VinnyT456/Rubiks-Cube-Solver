488a2999f80041a610e3cf167a9654c039d648c7
Vinny456 <vincent03280608@gmail.com>
2025-09-10 18:41:55 -0500

    Added the functions to turn each solutions into words"

--- solution.py
+++ solution.py
@@ -25,5 +25,48 @@ algorithm_word_conversion = {
     "y2":"Rotate the whole cube twice in any direction",
 }
 
-def algorithm_to_word(move):
-    pass
+def cross_solution_to_words(cross_solution: list[str]):
+    print("Cross Solution:")
+    for i in range(len(cross_solution)):
+        move = cross_solution[i]
+        print(f"{i+1}. {algorithm_word_conversion[move]}")
+    print()
+
+def corner_solution_to_words(corner_solution: list[list[str]]):
+    for i in range(len(corner_solution)):
+        print(f"Corner Solution {i}:")
+        for j in range(len(corner_solution[i])):
+            move = corner_solution[i][j]
+            print(f"{i+1}. {algorithm_word_conversion[move]}")
+        print()
+    print()
+
+def second_layer_edge_solution_to_words(edge_solution: list[list[str]]):
+    for i in range(len(edge_solution)):
+        print(f"Edge Solution {i}:")
+        for j in range(len(edge_solution[i])):
+            move = edge_solution[i][j]
+            print(f"{i+1}. {algorithm_word_conversion[move]}")
+        print()
+    print()
+
+def last_layer_cross_solution_to_words(last_layer_cross_solution: list[str]):
+    print("Last Layer Cross Solution:")
+    for i in range(len(last_layer_cross_solution)):
+        move = last_layer_cross_solution[i]
+        print(f"{i+1}. {algorithm_word_conversion[move]}")
+    print()
+
+def oll_solution_to_words(oll_solution: list[str]):
+    print("OLL Solution:")
+    for i in range(len(oll_solution)):
+        move = oll_solution[i]
+        print(f"{i+1}. {algorithm_word_conversion[move]}")
+    print()
+
+def pll_solution_to_words(pll_solution: list[str]):  
+    print("PLL Solution:")
+    for i in range(len(pll_solution)):
+        move = pll_solution[i]
+        print(f"{i+1}. {algorithm_word_conversion[move]}")
+    print()
